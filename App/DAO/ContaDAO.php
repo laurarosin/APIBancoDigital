c0d73e83f8f5f6fd16ec6f4b8ce7c5a23e4a05aa
laurarosin <laurarosin29@gmail.com>
2023-08-10 15:54:44 -0300

    kkk

--- App/DAO/ContaDAO.php
+++ App/DAO/ContaDAO.php
@@ -21,31 +21,31 @@ class ContaDAO extends DAO
         return $stmt->fetchAll(DAO::FETCH_CLASS);
     }
 
-    public function insert(ContaModel $m) : bool
+    public function insert(ContaModel $model) : bool
     {
         $sql = "INSERT INTO conta (Numero,Tipo,Senha,limite,saldo) VALUES (?, ?, ?, ?, ?) ";
 
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $m->Numero);
-        $stmt->bindValue(2, $m->Tipo);
-        $stmt->bindValue(3, $m->Senha);
-        $stmt->bindValue(4, $m->limite);
-        $stmt->bindValue(5, $m->saldo);
+        $stmt->bindValue(1, $model->Numero);
+        $stmt->bindValue(2, $model->Tipo);
+        $stmt->bindValue(3, $model->Senha);
+        $stmt->bindValue(4, $model->limite);
+        $stmt->bindValue(5, $model->saldo);
         
         return $stmt->execute();
     }
 
-    public function update(ContaModel $m)
+    public function update(ContaModel $model)
     {
         $sql = "UPDATE conta SET Numero=?, Tipo =?, Senha=? limite=? saldo=? WHERE Id=? ";
 
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $m->Numero);
-        $stmt->bindValue(2, $m->Tipo);
-        $stmt->bindValue(3, $m->Senha);
-        $stmt->bindValue(4, $m->limite);
-        $stmt->bindValue(5, $m->saldo);
-        $stmt->bindValue(6, $m->Id);
+        $stmt->bindValue(1, $model->Numero);
+        $stmt->bindValue(2, $model->Tipo);
+        $stmt->bindValue(3, $model->Senha);
+        $stmt->bindValue(4, $model->limite);
+        $stmt->bindValue(5, $model->saldo);
+        $stmt->bindValue(6, $model->Id);
         
         return $stmt->execute();
     }
